41a9d0b7475f15b23b8aa09c7be9510e84fd0cf1
udayuppar <udayr.xwokz@gmail.com>
2022-09-13 11:15:11 +0530

    files

---
+++ FridgeRun.java
@@ -0,0 +1,26 @@
+class FridgeRun{
+	public static void main(String[] as)
+	{
+		System.out.println("Fridge details");
+		
+		String name=fridge.name;
+		String colour=fridge.colour;
+		String power=fridge.power;
+		String model=fridge.model;
+		int doors=fridge.doors;
+		int capacity=fridge.capacity;
+		boolean stabilizer=fridge.stabilizer;
+		boolean frezer=fridge.frezer;
+		
+		
+		System.out.println("name of the fridge:"+name);
+		System.out.println("colour of the fridge:"+colour);
+		System.out.println("power consuming:"+power);
+		System.out.println("model name:"+model);
+		System.out.println("no of doors:"+doors);
+		System.out.println("capacity of the fridge:"+capacity);
+		System.out.println("it has a stabilizer:"+stabilizer);
+		System.out.println("it has a freezer:"+frezer);
+		
+	}
+}
---
+++ fridge.java
@@ -0,0 +1,10 @@
+class fridge{
+	static String name="LG";
+	static String colour="brown";
+	static String power="fiveStar";
+	static String model="sdu97mnw36vw52";
+	static int doors=1;
+	static int capacity=47;
+	static boolean stabilizer=true;
+    static boolean frezer=true;
+}
